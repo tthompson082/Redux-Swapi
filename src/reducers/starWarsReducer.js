a2279fd70aacd91d21223eb64349e3d5e6309460
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-19 15:17:47 -0400

    wired up index.js and reducers

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
 import /* we need our action types here*/ "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,24 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {
+        characters: [],
+        error: null,
+        isFetching: true
+      };
+      case SUCCESS:
+        return {
+          characters: [],
+          isFetching: false,
+          error: null
+        };
+        case FAILURE:
+          return {
+            characters: [],
+            error: 'There was an error in retrieving the requested information',
+            isFetching: false
+          }
     default:
       return state;
   }
